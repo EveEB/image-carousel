b2307a9f8dccb2a303b3ea4c026ebf52d3951e70
EveEB <104207900+EveEB@users.noreply.github.com>
2022-08-06 17:47:39 +0100

    Initial Commit

---
+++ src/images.js
@@ -0,0 +1,7 @@
+import cake from "./assets/cake.jpg";
+import norwich from "./assets/norwich.jpg";
+import scone from "./assets/scone2.jpg";
+import street from "./assets/street.jpg";
+import tea from "./assets/tea-and-cake.jpg";
+
+export default [cake, norwich, scone, street, tea];
